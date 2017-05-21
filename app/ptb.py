de71d5bc9112e2fcf9d460164921b020e79649e3
Jiayu <lijy.lijy@gmail.com>
2017-05-21 14:58:41 +1000

    see description
    - added pivot table page
    - connected ptb form to server

---
+++ app/ptb.py
@@ -0,0 +1,4 @@
+
+
+FILENAME = "dataset.csv"
+
--- app/server.py
+++ app/server.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 import csv
 from jinja2 import Template
+from ptb import *
 app = Flask(__name__, static_folder='.', static_url_path='')
 
 
@@ -32,21 +33,64 @@ def pivotTableBuilder():
     #Read cols
     f = open("assets/dataset.csv")
     csvreader = csv.reader(f, delimiter=',', skipinitialspace=True)
-    data=[]
+    attr = []
+    attr_trimmed = []
     for row in csvreader:
-        data.append(row)
+        attr = row
+        break
     f.close()
     
+    for a in attr:
+        attr_trimmed.append(a.replace(" ", "").lower())
     # Render data to html template
     template = Template(open('ptb.html').read())
     return template.render(
-        data = data
+        attr = attr, 
+        attr_trimmed = attr_trimmed, 
+        attr_len = len(attr)
     )
 
 
-@app.route("/pivotTable")
+@app.route("/pivotTable", methods=['GET', 'POST'])
 def pivotTable():
-    return "I'm a pivot table"
+    row = request.form['row']
+    col = request.form['col']
+    aggr_m = request.form['aggregation_method']
+    aggr_a = request.form['aggregation_attr']
+    filter_a = request.form['filter_attr']
+    filter_cond = request.form['filter_cond']
+    filter_val = request.form['filter_val']
+
+    # trimmed attribute to original attribute
+    t2o = {
+        "dateofstop": "Date Of Stop", 
+        "timeofstop": "Time Of Stop", 
+        "description": "Description", 
+        "belts": "Belts", 
+        "personalinjury": "Personal Injury", 
+        "commerciallicense": "Commercial License", 
+        "commercialvehicle": "Commercial Vehicle", 
+        "vehicletype": "Vehicle Type", 
+        "year": "Year", 
+        "make": "Make", 
+        "model": "Model", 
+        "color": "Color", 
+        "race": "Race", 
+        "gender": "Gender", 
+        "geolocation": "Geolocation"
+    }
+
+    if filter_val=="":
+        ptb_str = 'Row: ' + t2o[str(row)] + '; ' + 'Column: ' + t2o[str(col)] + '<br>' + 'Aggregation method: ' + str(aggr_m) + '; ' + 'Aggregation attribute: ' + t2o[str(aggr_a)] + '<br>' + 'Filter attribute: ' + t2o[str(filter_a)] + '; ' + 'Filter method: ' + str(filter_cond) + '; ' + 'Filter value: None<br>'
+    else:
+        ptb_str = 'Row: ' + t2o[str(row)] + '; ' + 'Column: ' + t2o[str(col)] + '<br>' + 'Aggregation method: ' + str(aggr_m) + '; ' + 'Aggregation attribute: ' + t2o[str(aggr_a)] + '<br>' + 'Filter attribute: ' + t2o[str(filter_a)] + '; ' + 'Filter method: ' + str(filter_cond) + '; ' + 'Filter value: ' + str(filter_val) + '<br>'
+    
+    #
+
+    template = Template(open('pivotTable.html').read())
+    return template.render(
+        ptb_str = ptb_str
+    )
 
 
 @app.route("/visualisation")
