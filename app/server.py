74edd5ed90f8ca92d856d9b3f6f41e7d2f3403ac
Jiayu <lijy.lijy@gmail.com>
2017-05-21 13:19:01 +1000

    ptb <- jinja2

--- app/server.py
+++ app/server.py
@@ -8,6 +8,7 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 def root():
     return app.send_static_file('index.html')
 
+
 @app.route("/rawData")
 def rawData():
     # Read data
@@ -36,72 +37,17 @@ def pivotTableBuilder():
         data.append(row)
     f.close()
     
-    body = """<body id="bg-img2">
-    <h2>Pivot Table Builder</h2>
-    <div class="form-group col-md-6 selector">
-        <label for="sel1">Report Filter</label>
-        <select class="form-control">"""
-        
-    for attr in data[0]:
-        body += "<option>"+attr+"</option>"
-        
-    body +="""
-        </select>
-        <select class="form-control">
-            <option>is anything</option>
-            <option>=</option>
-            <option>></option>
-            <option>>=</option>
-            <option><</option>
-            <option><=</option>
-            <option>!=</option>
-            <option>contains</option>
-            <option>doesn't contain</option>
-            <option>is Yes</option>
-            <option>is No</option>
-        </select>
-        <input class="form-control">
-    </div>  
-    <div class="form-group col-md-6 selector">
-        <label for="sel1">Row Label</label>
-        <select class="form-control">
-        <option>is No</option>
-        </select>
-    </div>
-    <div class="form-group col-md-6 selector">
-        <label for="sel1">Column Label</label>
-        <select class="form-control">"""
-    for attr in data[0]:
-        body += "<option>"+attr+"</option>"
-        
-    body +="""
-        </select>
-    </div>
-    <div class="form-group col-md-6 selector">
-        <label for="sel1">Aggregation</label>
-        <select class="form-control">
-            <option>count of</option>
-            <option>sum of</option>
-            <option>average of</option>
-            <option>maximum of</option>
-            <option>minimun of</option>
-        </select>
-        <select class="form-control">"""
-    for attr in data[0]:
-        body += "<option>"+attr+"</option>"
-    body +="""
-        </select>
-    </div>
-     <form>
-        <input type="button" class="btn btn-primary" onclick="location.href='/pivotTable';" value="Pivot Table" />
-    </form>>
-</body>"""
-    
-    return htmlTemplate("Pivot Table Builder", body)
+    # Render data to html template
+    template = Template(open('ptb.html').read())
+    return template.render(
+        data = data
+    )
+
 
 @app.route("/pivotTable")
 def pivotTable():
-    return
+    return "I'm a pivot table"
+
 
 @app.route("/visualisation")
 def visualisation():
